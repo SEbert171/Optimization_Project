e5206c85c40ddee621821ee54c739c6167d5cd01
Seval56 <123495526+Seval56@users.noreply.github.com>
2023-02-07 17:06:33 +0100

    Add files via upload

---
+++ maini.m
@@ -0,0 +1,25 @@
+clc;
+
+import casadi.*
+opti = casadi.Opti();
+
+X = opti.variable();
+Y = opti.variable();
+
+gamma=10000;
+
+f = -20*exp(-0.2*sqrt(0.5*(X^2+Y^2+1e-6)))-exp(0.5*(cos(2*pi*X)+cos(2*pi*Y)))+exp(1)+20; % ackley function
+g = X^2+Y^2-25; % constraint function for ackley
+% F=f+gamma*g^2;
+
+opti.minimize(f)
+ 
+opti.subject_to(g==0)
+
+opti.solver('ipopt');
+sol = opti.solve();
+ 
+x=sol.value(X);
+y=sol.value(Y);
+
+disp([x;y]);
